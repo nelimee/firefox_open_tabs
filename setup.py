2a0a6f2d94fc16e1ce2d163a561310b008d24547
Adrien Suau <adrien.suau@protonmail.com>
2019-08-08 17:58:08 +0200

    Added description in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="firefox_open_tabs",
     version="0.1.0",
+    description="Get information about currently opened tabs in Firefox",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     packages=["firefox_open_tabs"],
     url="https://github.com/nelimee/firefox_open_tabs",
     license="CeCILL-B",
     author="Adrien Suau",
     author_email="adrien.suau@protonmail.com",
-    description="Get information about currently opened tabs in Firefox",
     install_requires=["lz4"],
-    extras_require={"dev": ["black", "setuptools", "wheel"]},
+    extras_require={"dev": ["black", "setuptools", "wheel", "twine"]},
     entry_points={
         "console_scripts": ["firotab = firefox_open_tabs.firefox_open_tabs:main"]
     },
